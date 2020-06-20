8eec3aa7eb930ab30864a67f352b9ee6075e1337
betorresmo <mr.zetkix@gmail.com>
2020-06-20 10:21:20 -0300

    Add/Server Provider Appointment Listing

---
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments of a provider in a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      user_id: 'user-id',
+      provider_id: 'provider-id',
+      date: new Date(2020, 5, 1, 10, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      user_id: 'user-id',
+      provider_id: 'provider-id',
+      date: new Date(2020, 5, 1, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      year: 2020,
+      month: 6,
+      day: 1,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
