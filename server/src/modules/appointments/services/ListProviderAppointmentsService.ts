021814f05a6f1ac4684ea7d3e62e7af0fddfe37b
betorresmo <mr.zetkix@gmail.com>
2020-08-30 13:46:04 -0300

    Add/Server Appointments list cache

--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 interface IRequest {
   provider_id: string;
   year: number;
@@ -15,6 +17,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,6 +28,14 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
+    const appointmentsInCache = await this.cacheProvider.retrieve<
+      Appointment[]
+    >(`appointments-list:${year}-${month}-${day}:${provider_id}`);
+
+    if (appointmentsInCache) {
+      return appointmentsInCache;
+    }
+
     const appointments = await this.appointmentsRepository.findByProviderAndDay(
       {
         provider_id,
@@ -30,6 +43,11 @@ class ListProviderAppointmentsService {
         month,
         day,
       },
+    );
+
+    await this.cacheProvider.store(
+      `appointments-list:${year}-${month}-${day}:${provider_id}`,
+      appointments,
     );
 
     return appointments;
