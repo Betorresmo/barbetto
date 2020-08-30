5bf0c9d0074fdf4af10475a86f174f73b59b297f
betorresmo <mr.zetkix@gmail.com>
2020-08-30 14:08:07 -0300

    Add/Server Fake Cache Provider

---
+++ server/src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
@@ -0,0 +1,37 @@
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
+interface ICacheData {
+  [key: string]: string;
+}
+
+class FakeCacheProvider implements ICacheProvider {
+  private cache: ICacheData = {};
+
+  public async store(key: string, value: any): Promise<void> {
+    this.cache[key] = JSON.stringify(value);
+  }
+
+  public async retrieve<T>(key: string): Promise<T> {
+    const value = this.cache[key];
+
+    const parsedValue = JSON.parse(value) as T;
+
+    return parsedValue;
+  }
+
+  public async invalidateOne(key: string): Promise<void> {
+    delete this.cache[key];
+  }
+
+  public async invalidateMultiple(keyPrefix: string): Promise<void> {
+    const keys = Object.keys(this.cache).filter(key =>
+      key.startsWith(`${keyPrefix}:`),
+    );
+
+    keys.forEach(key => {
+      delete this.cache[key];
+    });
+  }
+}
+
+export default FakeCacheProvider;
