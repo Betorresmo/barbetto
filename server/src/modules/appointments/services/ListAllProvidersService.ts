306f2b09fd6257333334d561424029e50f58e07d
betorresmo <mr.zetkix@gmail.com>
2020-08-30 13:23:45 -0300

    Add/Server Providers list cache

--- server/src/modules/appointments/services/ListAllProvidersService.ts
+++ server/src/modules/appointments/services/ListAllProvidersService.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 interface IRequest {
   user_id: string;
 }
@@ -12,12 +14,24 @@ class ListAllProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async run({ user_id }: IRequest): Promise<User[]> {
+    const providersInCache = await this.cacheProvider.retrieve<User[]>(
+      `providers-list:${user_id}`,
+    );
+    if (providersInCache) {
+      return providersInCache;
+    }
+
     const providers = await this.usersRepository.findAll({
       except_user_id: user_id,
     });
+
+    await this.cacheProvider.store(`providers-list:${user_id}`, providers);
 
     return providers;
   }
